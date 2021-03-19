9aee154c34eeee833652601166013816c0719b92
samir patil <samirspatil742099@gmail.com>
2021-03-19 17:42:53 +0530

    graph

--- Graph/18_IterativeDepthFirstSearch_.py
+++ Graph/18_IterativeDepthFirstSearch_.py
@@ -1,5 +1,3 @@
-
-
 class Node:
     def __init__(self,src,nbr,wt):
         self.src=src
@@ -17,15 +15,17 @@ class Graph:
         node.next=self.graph[src]
         self.graph[src]=node
          
-        # node=Node(nbr,src,wt)
-        # node.next=self.graph[nbr]
-        # self.graph[nbr]=node
+        node=Node(nbr,src,wt)
+        node.next=self.graph[nbr]
+        self.graph[nbr]=node
     
     def print_graph(self):
+        # print(self.vertices)
         for i in range(self.vertices):
             print(f"head({i})",end=" ")
             temp=self.graph[i]
             while(temp):
+                # print("->",temp.wt)
                 print(f"--> {temp.src}-{temp.nbr} W {temp.wt}",end=" ")
                 temp=temp.next
             print()
@@ -45,32 +45,33 @@ graph_.add_edge(0,3,10)
 graph_.add_edge(0,1,10)
 graph_.add_edge(1,2,10)
 graph_.add_edge(2,3,10)
-graph_.add_edge(4,3,10)
+graph_.add_edge(3,4,10)
 graph_.add_edge(4,6,10)
 graph_.add_edge(4,5,10)
 graph_.add_edge(5,6,10)
 graph_.print_graph()
 
 
-visited=[False for _ in range(v+1)]
+bool_arr=[False for i in range(v+1)]
 
-def findDFS(graph,src,st,visited):
-    visited[src]=True
-    edge=graph.get_node(src)
-    while(edge):
-        if(visited[edge.nbr]==False):
-            findDFS(graph,edge.nbr,st,visited)
-        edge=edge.next
-    st.append(src)
+
+def IterativeDepthFirstSearch(graph,src,visited):
+    st=[]
+    st.append([src,str(src)+""])
+    while(len(st)>0):
+        rem=st.pop()
+        if(visited[rem[0]]==True):
+            continue
+        visited[rem[0]]=True
+        print(rem[0],"--",rem[1])
+        edge=graph.get_node(rem[0])
+        while(edge):
+            if(visited[edge.nbr]==False):
+                st.append([edge.nbr,rem[1]+str(edge.nbr)])
+            edge=edge.next
     
-    
 
-st=[]
+IterativeDepthFirstSearch(graph_,0,bool_arr)   
 
-for i in range(v+1):
-    if(visited[i]==False):
-        findDFS(graph_,i,st,visited)
+
         
-st.reverse()
-print(st)
-
---
+++ Python/3_0_stack__LL.py
@@ -0,0 +1,21 @@
+# Python program to 
+# demonstrate stack implementation
+# using list
+
+
+stack = []
+stack.append(1)
+stack.append(2)
+stack.append(3)
+
+print('Initial stack')
+print(stack)
+print('\nElements poped from stack:')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print('\nStack after elements are poped:')
+print(stack)
+
+
